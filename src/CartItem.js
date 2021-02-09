3c2b96fc41c882b9b8024f1b8f9c8630afc87f86
DivYaM <divyamsaxena123@gmail.com>
2021-02-10 01:46:34 +0530

    Added CartItem

---
+++ src/CartItem.js
@@ -0,0 +1,34 @@
+import  React  from "react";
+
+class CartItem extends React.Component{
+    render(){
+        return(
+            <div className="cart-item">
+                <div className="left-block">
+                    <img style={styles.image} />
+                </div>
+                <div className="right-block">
+                    <div style={{fontSize: 25}}>Phone</div>
+                    <div style={{color: 'gray'}}>Rs 999</div>
+                    <div style={{color: 'gray'}}>Qty: 1</div>
+                    <div className="cart-item-actions">
+                        {/*  Button  */}
+                    </div>
+                </div>
+            </div>
+        );
+    }
+}
+    
+
+const styles = {
+    image : {
+        height: 110,
+        width: 110,
+        borderRadius: 4,
+        background: '#ccc'
+    }
+}
+
+
+export  default CartItem;
