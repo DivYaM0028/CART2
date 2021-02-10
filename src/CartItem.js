25a588889cc67b6971a1a87a244430a5ab6e9c34
DivYaM <divyamsaxena123@gmail.com>
2021-02-11 02:05:41 +0530

    Add increase Qty function

--- src/CartItem.js
+++ src/CartItem.js
@@ -11,6 +11,10 @@ class CartItem extends React.Component{
         }
     }
 
+    increaseQuantity = () =>{
+        this.state
+    }
+
     render(){
         return(
             <div className="cart-item">
@@ -23,9 +27,20 @@ class CartItem extends React.Component{
                     <div style={{color: 'gray'}}>Qty: {this.state.qty}</div>
                     <div className="cart-item-actions">
                         {/*  Button  */}
-                        <img alt="increase" className="action-icons" src="https://www.flaticon.com/svg/vstatic/svg/992/992651.svg?token=exp=1612903022~hmac=8b1c3de089f225b770cfe8c9c0a38154" />
-                        <img alt="decrease" className="action-icons" src="https://www.flaticon.com/svg/vstatic/svg/992/992683.svg?token=exp=1612902963~hmac=bf50674783cde6b835eeb4b7d8b5b634" />
-                        <img alt="delete" className="action-icons" src="https://www.flaticon.com/svg/vstatic/svg/3096/3096673.svg?token=exp=1612903073~hmac=8ae1de8585b3f061050d2ce124271d26" />
+                        <img 
+                         alt="increase"
+                         className="action-icons"
+                         src="https://www.flaticon.com/svg/vstatic/svg/992/992651.svg?token=exp=1612903022~hmac=8b1c3de089f225b770cfe8c9c0a38154" 
+                         onClick={this.increaseQuantity}    
+                        />
+                        <img 
+                         alt="decrease" 
+                         className="action-icons" 
+                         src="https://www.flaticon.com/svg/vstatic/svg/992/992683.svg?token=exp=1612902963~hmac=bf50674783cde6b835eeb4b7d8b5b634" />
+                        <img 
+                         alt="delete" 
+                         className="action-icons" 
+                         src="https://www.flaticon.com/svg/vstatic/svg/3096/3096673.svg?token=exp=1612903073~hmac=8ae1de8585b3f061050d2ce124271d26" />
                     </div>
                 </div>
             </div>
