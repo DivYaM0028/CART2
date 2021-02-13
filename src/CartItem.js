9f21c00843190d169680953fc8a111dce8cba627
DivYaM <divyamsaxena123@gmail.com>
2021-02-14 02:45:01 +0530

    Props

--- src/CartItem.js
+++ src/CartItem.js
@@ -1,15 +1,15 @@
 import  React  from "react";
 
 class CartItem extends React.Component{
-    constructor (){
-        super();
-        this.state ={
-            title : 'Phone',
-            price : 999,
-            qty : 1,
-            img : ''
-        }
-    }
+    // constructor (){
+    //     super();
+    //     this.state ={
+    //         title : 'Phone',
+    //         price : 999,
+    //         qty : 1,
+    //         img : ''
+    //     }
+    // }
 
     increaseQuantity = () =>{
         // setstate form 1
@@ -41,15 +41,16 @@ class CartItem extends React.Component{
 
 
     render(){
+        const { title , price ,qty } = this.props.product;
         return(
             <div className="cart-item">
                 <div className="left-block">
                     <img style={styles.image} />
                 </div>
                 <div className="right-block">
-                    <div style={{fontSize: 25}}>{this.state.title}</div>
-                    <div style={{color: 'gray'}}>Rs {this.state.price}</div>
-                    <div style={{color: 'gray'}}>Qty: {this.state.qty}</div>
+                    <div style={{fontSize: 25}}>{title}</div>
+                    <div style={{color: 'gray'}}>Rs {price}</div>
+                    <div style={{color: 'gray'}}>Qty: {qty}</div>
                     <div className="cart-item-actions">
                         {/*  Button  */}
                         <img 
